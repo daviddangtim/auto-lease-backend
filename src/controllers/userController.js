491c564193ca32be7fccc5078455e301d56616bb
daviddangtim <132236718+daviddangtim@users.noreply.github.com>
2024-06-26 11:53:09 +0100

    Logic for Updating name and email of user

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,20 +2,46 @@ import { catchAsync } from "../utils/utils.js";
 import User from "../models/userModel.js"
 
 
-
 export const updateMyPassword = catchAsync(async (req, res, next) => {
     const { password } = req.body;
-
+    const date = new Date()
     const user = await User.findById(req.user._id)
-    if (!user){
-        return res.status(404).json({message : "Internal Server Error"})
+
+    if (!user) {
+        return res.status(404).json({ message: "Internal Server Error" })
     }
-    user.password = password;
+
+    await Promise.all(() => {
+        user.passwordChangedAt = date.now();
+        user.password = password;
+    }
+    )
 
     await user.save()
-    
-    res.status(200).json({message:"Password Updated Successfully"})
+
+    res.status(200).json({ message: "Password Updated Successfully" })
 });
+
 export const updateProfilePhoto = catchAsync(async (req, res, next) => { });
-export const updateMe = catchAsync(async (req, res, next) => { });
+
+export const updateMe = catchAsync(async (req, res, next) => {
+    const updates = {}
+
+    if (req.body.name) {
+        updates.name = req.body.name
+    }
+
+    if (req.body.email) {
+        updates.email = req.body.email
+    }
+
+    const user = findByIdAndUpdate(req.user._id, { $set: { updates } })
+    if (user.nModified === 0) {
+        return res.status(404).json({ message: "Unable to update fields because they were not found" })
+    }
+
+    res.status(200).json({ message: "Details Updated Successfully" })
+    await user.save();
+});
+
 export const deleteMe = catchAsync(async (req, res, next) => { });
