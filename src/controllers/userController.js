fed8a519f1e409e41bd439b72aaff831e5b3e480
daviddangtim <132236718+daviddangtim@users.noreply.github.com>
2024-06-26 11:18:58 +0100

    Fixed the logic for Update Password

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -6,12 +6,15 @@ import User from "../models/userModel.js"
 export const updateMyPassword = catchAsync(async (req, res, next) => {
     const { password } = req.body;
 
-    const user = await User.findOne(req.user._id)
+    const user = await User.findById(req.user._id)
     if (!user){
         return res.status(404).json({message : "Internal Server Error"})
     }
-    await user.updateOne({ password: user.password }, { $set: { password } })
+    user.password = password;
 
+    await user.save()
+    
+    res.status(200).json({message:"Password Updated Successfully"})
 });
 export const updateProfilePhoto = catchAsync(async (req, res, next) => { });
 export const updateMe = catchAsync(async (req, res, next) => { });
