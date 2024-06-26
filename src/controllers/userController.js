2bd838707161434690c31e19f509324d3be04c48
daviddangtim <132236718+daviddangtim@users.noreply.github.com>
2024-06-26 11:15:12 +0100

    Changed one function in userModel to use arrow function(Minor change) Prototype of UpdatePassword function in userController

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,18 @@
 import { catchAsync } from "../utils/utils.js";
+import User from "../models/userModel.js"
 
-export const updateMyPassword = catchAsync(async (req, res, next) => {});
-export const updateProfilePhoto = catchAsync(async (req, res, next) => {});
-export const updateMe = catchAsync(async (req, res, next) => {});
-export const deleteMe = catchAsync(async (req, res, next) => {});
+
+
+export const updateMyPassword = catchAsync(async (req, res, next) => {
+    const { password } = req.body;
+
+    const user = await User.findOne(req.user._id)
+    if (!user){
+        return res.status(404).json({message : "Internal Server Error"})
+    }
+    await user.updateOne({ password: user.password }, { $set: { password } })
+
+});
+export const updateProfilePhoto = catchAsync(async (req, res, next) => { });
+export const updateMe = catchAsync(async (req, res, next) => { });
+export const deleteMe = catchAsync(async (req, res, next) => { });
