a0b594d0cbc794091a71e5fe292b43ce531b1e4c
daviddangtim <132236718+daviddangtim@users.noreply.github.com>
2024-06-26 10:37:21 +0100

    Added Car Model, Mainly just boilerplate

---
+++ src/models/carsModel.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose"
+
+const carSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      unique: [true, "This name is already in use"],
+      trim: true,
+      minlength: [4, "Dealership Name must be at least 4 characters long"],
+      maxlength: [50, "Name cannot be more than 50 characters"],
+      validate: {
+        validator: (value) => /^[a-zA-Z][a-zA-Z0-9]*$/.test(value),
+        message:
+          "Name must start with a letter and can only contain letters and numbers without spaces or special characters",
+      },
+    },
+    numRatings: {
+      type: Number,
+      default: 0,
+      min:[]
+    },
+    ratingsAverage: {
+      type: Number,
+    }
+  },
+  { timestamps: true },
+);
+
+const Dealership = mongoose.model("Dealership", dealershipSchema);
