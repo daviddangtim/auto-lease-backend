9ef6515b70a24e0abf43bce68b829ef7c5a21e8d
daviddangtim <132236718+daviddangtim@users.noreply.github.com>
2024-06-27 23:34:16 +0100

    Improved the image uploader by adding file size limits and type limits : yet to test in postman

--- src/utils/imageUploader.js
+++ src/utils/imageUploader.js
@@ -1,6 +1,7 @@
 import multer from "multer";
 import {v2 as cloudinary} from "cloudinary";
 import dotenv from "dotenv";
+import AppError from "./appError.js";
 
 dotenv.config();
 
@@ -9,14 +10,25 @@ cloudinary.config({
     api_key : process.env.CLOUDINARY_KEY,
     api_secret : process.env.CLOUDINARY_SECRET
 });
+const maxSize = 2*1000*1000;
+const filetype =  file => { file.mimetype.toString()};
+
 
 const storage= multer.diskStorage({
     filename: (req,file,cb)=>{
-    cb(file.originalname)
-}
-})
+        cb(file.originalname)
+    },
 
-const upload = multer({storage});
+})
+const fileFilter =  (req,file,cb)=> {
+    if (filetype === "image/png" || filetype === "image/jpg" || filetype === "image/jpeg") {
+        cb(null, true)
+    } else {
+        cb(new AppError("File type not allowed", 400), false)
+    }
+}
+
+const upload = multer({storage,limits:{fileSize:maxSize},fileFilter:fileFilter});
 
 export {upload, cloudinary}
 
