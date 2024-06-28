754035752ce443989c88a73756e04275b16673d3
agionoja <pauldivineagionoja@gmail.com>
2024-06-28 16:37:47 +0100

    Chore: fixed merged conflict

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,20 +1,44 @@
 import { catchAsync } from "../utils/utils.js";
+import User from "../models/userModel.js";
 import AppError from "../utils/appError.js";
 
 export const updateMyPassword = catchAsync(async (req, res, next) => {
-  const { currentPassword, password, passwordConfirm } = req.body;
-  const { user } = req;
+  const { password } = req.body;
+  const date = new Date();
+  const user = await User.findById(req.user._id);
 
-  if (!(await user.comparePassword(currentPassword, user.password))) {
-    return next(new AppError("Current password is incorrect", 400));
+  if (!user) {
+    return res.status(404).json({ message: "Internal Server Error" });
   }
-  user.password = password;
-  user.passwordConfirm = passwordConfirm;
+
+  await Promise.all(() => {
+    // WHY ARE YOU USING A PROMISE HERE?
+    user.passwordChangedAt = date.now(); // WE DONT NEED THIS BECAUSE BECAUSE IT'S HANDLES BY A MIDDLEWARE
+    user.password = password;
+  });
 
   await user.save();
 
+  res.status(200).json({ message: "Password Updated Successfully" });
+});
+
+export const updateMyPasswordV1 = catchAsync(async (req, res, next) => {
+  // GET THE CURRENT USER FROM THE REQUEST OBJECT PASSED BY PROTECT
+  const { user } = req;
+  const { currentPassword, password, passwordConfirm } = req.body;
+
+  // COMPARE THE PASSWORDS
+  if (!(await user.comparePassword(currentPassword, user.password))) {
+    return next(new AppError("Current password is incorrect", 401));
+  }
+
+  // UPDATE THE PASSWORD
+  user.password = password;
+  user.passwordConfirm = passwordConfirm;
+  await user.save(); // IF VALIDATION FAILS OUR GLOBAL HANDLING MIDDLEWARE WILL TAKE OVER
+
   res.status(200).json({
-    statusText: "Password Updated Successfully", // changed message to statusTExt for consistency
+    statusText: "success",
     data: { user },
   });
 });
