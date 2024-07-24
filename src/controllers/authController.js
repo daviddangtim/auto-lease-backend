a1641bad49e0e7d76717975a74e319759d2a25eb
daviddangtim <132236718+daviddangtim@users.noreply.github.com>
2024-07-25 00:30:49 +0100

    User

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -29,7 +29,7 @@ export const verifyUser = catchAsync(async (req, res, next) => {
 export const signIn = catchAsync(async (req, res, next) => {
   const { password, email } = req.body;
   const { message} = await authService.signIn(password, email);
-  await generateAndSendJwtCookie(res, user, 201, message);
+  await generateAndSendJwtCookie(res, message.user, 201, message);
 
   res.status(200).json({
     statusText: "success",
