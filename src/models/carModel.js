28cb13a6533c8470e6ccd590453e4c5ad29bf485
daviddangtim <dangtimdavid021@gmail.com>
2024-07-30 02:26:44 +0100

    Fixes?

--- src/models/carModel.js
+++ src/models/carModel.js
@@ -28,15 +28,15 @@ const carSchema = new mongoose.Schema(
       minlength: [3, "A car name must be at least 3 characters long"],
       maxlength: [50, "Name cannot be more than 50 characters"],
     },
-    plateNumber: {
-      type: String,
-      trim: true,
-      required: [true, "Plate Number is required"],
-      match: [
-        /^[A-Z]{3}-?\d{3}[A-Z]{2}$/,
-        "Please enter a valid Nigerian plate number",
-      ],
-    },
+    // plateNumber: {
+    //   type: String,
+    //   trim: true,
+    //   required: [true, "Plate Number is required"],
+    //   match: [
+    //     /^[A-Z]{3}-?\d{3}[A-Z]{2}$/,
+    //     "Please enter a valid Nigerian plate number",
+    //   ],
+    // },
 
     category: {
       type: String,
@@ -85,18 +85,18 @@ const carSchema = new mongoose.Schema(
       required: [true, "A car must have a price"],
       min: [0, "Price cannot be negative"],
     },
-    fee: {
-      type: Number,
-      required: [true, "A car must have a lease fee"],
-      validate: {
-        validator: function (value) {
-          const price =
-            this.price !== undefined ? this.price : this.get("price");
-          return price > value;
-        },
-        message: "Fee must be less than the price",
-      },
-    },
+    // fee: {
+    //   type: Number,
+    //   required: [true, "A car must have a lease fee"],
+    //   validate: {
+    //     validator: function (value) {
+    //       const price =
+    //         this.price !== undefined ? this.price : this.get("price");
+    //       return price > value;
+    //     },
+    //     message: "Fee must be less than the price",
+    //   },
+    // },
     discount: {
       type: Number,
       default: 0,
